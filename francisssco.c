6d4c104f070d12f7fd477caa55907b1c0f455e01
Francisquinho arrasta crença <emcrivelmente318@gmail.com>
2024-09-24 11:13:18 -0300

    Add files via upload
    nome, letra e soma

---
+++ francisssco.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int n1= 6; //essa variável a
+int n2= 20;
+int soma;
+char letra = 'A';
+char nome[10]={"aphonsinha"};
+printf("%c",letra);
+printf ("%s",nome)
+soma = n1 + n2;
+printf("%d",soma);
+
+}
