51148ef62ee3e0b122a0b854930c7e33534c3d0c
Francisquinho arrasta crença <emcrivelmente318@gmail.com>
2024-10-08 09:31:02 -0300

    Add files via upload

---
+++ atividade2.c
@@ -0,0 +1,16 @@
+#include <math.h>
+int main (){
+ float capital;
+ float juros;
+ float meses;
+ printf("valor");
+ scanf("%f", &capital);
+ printf("porcentagem de juros por mês");
+ scanf("%f", &juros);
+ printf("quantidade de meses");
+ scanf("%f", &meses);
+ float resultado = capital*( 1+juros/100);
+ pow ; resultado, meses;
+ printf("%f", pow(resultado, meses));
+ 
+ }
---
+++ atividade4.c
@@ -0,0 +1,12 @@
+#include <math.h>
+
+int main (){
+    int idadeFernando = 18;
+    int idadeDanilo = 58;
+    if(idadeFernando > idadeDanilo){
+        printf("Fernando is oldest");
+    }
+    else
+    {
+printf("Danilo é mais velho");}
+}
