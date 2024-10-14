caf28c1521bc9155f238504c48df73394db76972
Francisquinho arrasta crença <emcrivelmente318@gmail.com>
2024-10-14 12:49:31 -0300

    Add files via upload
    3.1, INCOMPLETO

---
+++ papelariabb.c
@@ -0,0 +1,22 @@
+#include <math.h>
+
+int main () {
+
+    float preço1 = 0.25;
+    float preço2 = 0.20;
+    float quantidade_de_folhas;
+
+    printf("quantidade_de_folhas");
+    scanf("%f", &quantidade_de_folhas);
+    
+    float multiplicação1 = preço1*quantidade_de_folhas;
+    float multiplicação2 = preço2*quantidade_de_folhas;
+    if(quantidade_de_folhas <= 100){
+       float printf("multiplicação1");}
+       else
+       {
+        printf("multiplicação2");
+
+
+    }
+}
