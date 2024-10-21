b5c315b07827f30576353c0a15cd662847bb6aba
Francisquinho arrasta crença <emcrivelmente318@gmail.com>
2024-10-21 09:14:20 -0300

    Add files via upload

---
+++ fluxograma.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main () {
+    char estado_civil;
+
+    printf("insira uma letra mor:  ");
+    scanf("%c", &estado_civil);
+    if(estado_civil == s){
+        printf("solteiro");
+    }
+    if(estado_civil == c); {
+        printf("casadoh mor");
+    }
+    if(estado_civil == d);{
+        print("divorciado");
+    }
+    if(estado_civil) == v);{
+        print("viuvo");
+    }
+
+
+
+
+
+
+}
