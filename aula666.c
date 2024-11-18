55745c42baf2e42ab9a3cb0e832df880bdda056c
Francisquinho arrasta crença <emcrivelmente318@gmail.com>
2024-11-18 09:07:57 -0300

    Add files via upload

---
+++ aula666.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+int n, c, r;
+printf("Numero? ");
+scanf("%d", &n);
+printf("\nTabuada do %d\n\n", n);
+for(c=1 ; c<=10 ; c--) {
+r = n*c;
+printf("%d x %2d = %3d\n", n, c, r);
+}
+return 0;
+{
+    int n;
+}
+}
+
---
+++ for.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+ int c;
+
+ for(c=00; c<=225; c++){
+    printf("(%d,%c)\n ", c, c);
+ }
+ }
